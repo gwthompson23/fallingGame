362a928f92250c1177ea8a7029aa93a84efc49a3
gwthompson23 <gwthompson23@gmail.com>
2020-08-11 21:23:14 -0400

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject losePanel;
+    
+    public Text healthDisplay;
+    
+    public float speed;
+    private float input;
+
+    Rigidbody2D rb;
+    private Animator anim;
+
+    public int health;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        healthDisplay.text = health.ToString();
+    }
+
+    private void Update()
+    {
+        if (input != 0)
+        {
+            anim.SetBool("isRunning", true);
+        } else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (input > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else if (input < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Get input
+        input = Input.GetAxisRaw("Horizontal");
+
+        //Moving player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+        
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+        healthDisplay.text = health.ToString();
+        source.Play();
+
+        if (health <= 0)
+        {
+            losePanel.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
